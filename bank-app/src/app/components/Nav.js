69d1ad4275ebdab6308cbd826d8b29bb7d65898d
Yannis <arcorio.y@gmail.com>
2022-05-30 21:55:14 +0200

    add page with html/css only

--- bank-app/src/app/components/Nav.js
+++ bank-app/src/app/components/Nav.js
@@ -5,7 +5,7 @@ import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 const Nav = () => {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <a className="main-nav-logo" href="./">
         <img
           className="main-nav-logo-image"
           src="./img/argentBankLogo.png"
@@ -14,7 +14,7 @@ const Nav = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </a>
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
+        <a className="main-nav-item" href="./login">
         <FontAwesomeIcon icon={faUserCircle} />
           Sign In
         </a>
--- bank-app/src/app/pages/Login.js
+++ bank-app/src/app/pages/Login.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Login = () => {
-  return <section></section>;
+  return (
+    <div className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <a href="./user" className="sign-in-button">
+            Sign In
+          </a>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
